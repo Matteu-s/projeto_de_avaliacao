7de3ba5bdc7f5d46a843542323934f092d793d21
Mateus Santos <mateeussantoos@hotmail.com>
2023-11-10 08:23:19 -0300

    implementacao da gem devise + base user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[name email password password_confirmation cpf rg dob
+                                                         zip_code address address_number complement state city
+                                                         neighbourhood country mobile01 mobile02 phone01 phone02])
+  end
 end
