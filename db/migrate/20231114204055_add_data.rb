2ab6d7906b19863d50853e2d02b4576612a0a710
Mateus Santos <mateeussantoos@hotmail.com>
2023-11-15 16:24:13 -0300

    ajuste de data

--- db/migrate/20231114204055_add_data.rb
+++ db/migrate/20231114204055_add_data.rb
@@ -1,7 +1,8 @@
 class AddData < ActiveRecord::Migration[7.0]
   def change
-    #devido a falta de memoria ram do fly.io nao foi possível rodar rails db:seed
+    # devido a falta de memoria ram do fly.io nao foi possível rodar rails db:seed
     address_data = Correios::CEP::AddressFinder.get('50860260')
+    address_rdmapps = Correios::CEP::AddressFinder.get('50030-170')
 
     200.times do
       User.create(
@@ -49,15 +50,23 @@ class AddData < ActiveRecord::Migration[7.0]
     end
 
     1000.times do
+      state = %i[available unavailable maintenance].sample
+      if state == :unavailable
+        current_responsible = Faker::Name.name
+        delivery_date_responsible = DateTime.current - rand(1..6).months
+      else
+        current_responsible = nil
+        delivery_date_responsible = nil
+      end
       Equipment.create(
         name: Faker::Game.title,
         serial_number: Faker::Alphanumeric.alphanumeric(number: 10),
         date_of_acquisition: DateTime.current - rand(1..24).months,
-        cost: rand(10000.999999),
-        state: %i[available unavailable maintenance].sample,
+        cost: rand(10_000.999999),
+        state:,
         last_maintenance: DateTime.current - rand(1..24).months,
-        current_responsible: Faker::Name.name,
-        delivery_date_responsible: DateTime.current,
+        current_responsible:,
+        delivery_date_responsible:,
         company: Company.order('RANDOM()').first
       )
     end
@@ -82,5 +91,26 @@ class AddData < ActiveRecord::Migration[7.0]
       phone01: Faker::Number.number(digits: 10),
       phone02: Faker::Number.number(digits: 10)
     )
+
+    User.create(
+        name: 'Road Mapps',
+        email: 'rdmapps@rdmapps.com',
+        password: '123456',
+        password_confirmation: '123456',
+        cpf: Faker::Number.number(digits: 11),
+        rg: Faker::Number.number(digits: 8),
+        dob: DateTime.current - rand(1..99).year,
+        zip_code: address_rdmapps[:zipcode],
+        address: address_rdmapps[:address],
+        address_number: 125,
+        complement: '3 ANDAR',
+        state: address_rdmapps[:state],
+        city: address_rdmapps[:city],
+        neighborhood: address_rdmapps[:neighborhood],
+        mobile01: Faker::Number.number(digits: 11),
+        mobile02: Faker::Number.number(digits: 11),
+        phone01: Faker::Number.number(digits: 10),
+        phone02: Faker::Number.number(digits: 10)
+      )
   end
 end
